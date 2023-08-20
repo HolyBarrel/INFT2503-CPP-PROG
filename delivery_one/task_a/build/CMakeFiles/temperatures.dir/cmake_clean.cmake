023312918f3f59463bdfaedc7ebd93e274e77cd9
HolyBarrel <magnus.allison@getmail.no>
2023-08-20 15:57:46 +0000

    Tweaked some logic for task a p 1 + fixed subdirs

---
+++ delivery_one/task_a/build/CMakeFiles/temperatures.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/temperatures.dir/main.cpp.o"
+  "CMakeFiles/temperatures.dir/main.cpp.o.d"
+  "temperatures"
+  "temperatures.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/temperatures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ delivery_one/task_a/build/CMakeFiles/temperatures.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for temperatures.
+# This may be replaced when dependencies are built.
