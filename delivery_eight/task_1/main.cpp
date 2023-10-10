f5cbec0001381ef91099af8f6cfca7b39212d0dc
HolyBarrel <magnus.allison@getmail.no>
2023-10-10 11:48:44 +0200

    Added task 1 of delivery 8

--- delivery_eight/task_1/main.cpp
+++ delivery_eight/task_1/main.cpp
@@ -1,60 +1,47 @@
-#include "fraction.hpp"
 #include <iostream>
+#include <iomanip>
 #include <string>
 
 using namespace std;
 
-void print(const string &text, const Fraction &broek) {
-  cout << text << broek.numerator << " / " << broek.denominator << endl;
-}
+template <typename Type>
+bool equal(Type a, Type b);
+
+bool equal(double a, double b);
 
 int main() {
-  Fraction a(10, 20);
-  Fraction b(3, 4);
-  Fraction c;
-  c.set(5);
-  Fraction d = a / b;
 
-  print("a = ", a);
-  print("b = ", b);
-  print("c = ", c);
-  print("d = ", d);
+  int first = 1; 
+  int second = 2;
+  double third = 2.001;
+  string fourth = "1";
+  string fifth = "1";
+  double sixth = 0.01;
+  double seventh = 2.00100000001;
 
-  b += a;
-  ++c;
-  d *= d;
+  cout << "Testing with template version:" << boolalpha << endl << endl
+  << "var1 *datatype* \tvar2 *datatype* \tresult of equal(var1, var2) *bool*" << endl
+  << first << "  *int*\t\t" << second << "  *int*\t\t" << equal(first, second) << endl
+  << fourth << "  *string*\t\t" << fifth << "  *string*\t\t" << equal(fourth, fifth) << endl << endl;
 
-  print("b = ", b);
-  print("c = ", c);
-  print("d = ", d);
+  cout << "Testing with double version:" << boolalpha << endl << endl
+  << "var1 *datatype* \tvar2 *datatype* \tresult of equal(var1, var2) *bool*" << endl
+  << third << "  *double*\t\t" << fixed << setprecision(11) << seventh << "  *double*\t" << setprecision(3) << equal(third, seventh) << endl
+  << third << "  *double*\t\t" << sixth << "  *double*\t\t" << equal(third, sixth) << endl;
 
-  c = a + b - d * a;
-  c = -c;
-
-  print("c = ", c);
-
-  if (a + b != c + d)
-    cout << "a + b != c + d" << endl;
-  else
-    cout << " a + b == c + d" << endl;
-  while (b > a)
-    b -= a;
-  print("b = ", b);
-
-  //TASK 1
-  Fraction e(3, 2);
-  Fraction f(1, 5);
-
-  cout << endl << endl << "e - 1 = " << endl;
-  Fraction g = e - 1;
-
-  print("g = ", g);
-
-  cout << endl << "1 - f = " << endl;
-
-  Fraction h = 1 - f;
-
-  print("h = ", h);
 
   return 0;
+}
+
+template <typename Type>
+bool equal(Type a, Type b)
+{
+  //cout << endl << "The template version was used on the parameters " << a << " and " << b <<endl;
+  return a == b; //Based on the assumption in the task that the == operator is defined for the two types
+}
+
+bool equal(double a, double b)
+{
+  //cout << endl << "The double version was used on the parameters " << a << " and " << b << endl;
+  return abs(a-b) < 0.00001;
 }
